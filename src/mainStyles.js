29ef6e52c6c1019c06829241c89e54f80c1711a8
Vinícius Ernani de Carvalho <viniciusernanicarvalho@gmail.com>
2020-05-31 12:03:04 -0300

    Start creating the main styles

---
+++ src/mainStyles.js
@@ -0,0 +1,18 @@
+// Colors
+export const colors = {
+  white: "#FFFFFF",
+};
+
+// Dimensions
+export const dimensions = {
+  extraSmall: "5px",
+  small: "10px",
+  regular: "15px",
+  large: "20px",
+  extraLarge: "25px",
+};
+
+// Boundaries
+export const borders = {
+  thin: "1px solid rgba(78, 78, 78, 0.35)",
+};
