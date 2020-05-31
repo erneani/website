db7a715872918c8cbff4b11d4ae108ef1ab0adba
Vinícius Ernani de Carvalho <viniciusernanicarvalho@gmail.com>
2020-05-31 15:11:52 -0300

    Add new main styles

--- src/mainStyles.js
+++ src/mainStyles.js
@@ -1,6 +1,21 @@
+import styled from "styled-components";
+
+// Fonts
+export const fonts = {
+  sizes: {
+    small: "12px",
+    regular: "16px",
+    medium: "18px",
+    large: "24px",
+    extraLarge: "32px",
+  },
+};
+
 // Colors
 export const colors = {
+  primary: "#8000FF",
   white: "#FFFFFF",
+  black: "#2B2B2B",
 };
 
 // Dimensions
@@ -12,7 +27,46 @@ export const dimensions = {
   extraLarge: "25px",
 };
 
+export const ranges = {
+  extraSmall: "0.25rem",
+  small: "0.5rem",
+  regular: "1rem",
+  large: "1.5rem",
+  extraLarge: "2rem",
+  overwhelmingLarge: "4rem",
+};
+
+// Timings
+export const Timing = {
+  overwhelmingSmall: "0.1s",
+  small: "0.3s",
+  regular: "0.5s",
+  large: "0.7s",
+  veryLarge: "1s",
+};
+
 // Boundaries
 export const borders = {
   thin: "1px solid rgba(78, 78, 78, 0.35)",
+  rounded: "50px",
+};
+
+// Components
+export const CallToAction = styled.button`
+  background-color: ${colors.primary};
+  border-radius: ${borders.rounded};
+  transition: ${Timing.small};
+  padding: ${ranges.small} ${ranges.overwhelmingLarge};
+  border: 1px solid ${colors.primary};
+  color: ${colors.white};
+
+  &:hover {
+    background-color: color: ${colors.white};
+  }
+`;
+
+export const Texts = {
+  Primary: styled.span`
+    color: ${colors.primary};
+  `,
 };
