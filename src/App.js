dfa50a234321e94afcb1c72b4c68c037a5add8d0
Vinícius Ernani de Carvalho <viniciusernanicarvalho@gmail.com>
2020-05-31 16:32:22 -0300

    Add the header to the page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "./components/Header";
 import Showcase from "./sections/Showcase";
 import Jobs from "./sections/Jobs";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Showcase />
       <Jobs />
     </div>
