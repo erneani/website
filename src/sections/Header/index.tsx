94ca01a5070c6d57c26ae8625ef84f53924b768e
Vinícius Ernani de Carvalho <viniciusernanicarvalho@gmail.com>
2020-08-27 10:21:57 -0300

    Adjust header

--- src/sections/Header/index.tsx
+++ src/sections/Header/index.tsx
@@ -1,57 +1,97 @@
 import React from "react";
-import { Box } from "@material-ui/core";
+import styled from "styled-components";
+import { Box, Link } from "@material-ui/core";
 import { Colors } from "styles";
 
 import Logo from "assets/images/Logo.svg";
+
+interface isActivable {
+  isActive: boolean;
+}
 
 const anchors = [
   {
     title: "Início",
     url: "#showcase",
-    isSelected: false,
+    isActive: true,
   },
   {
     title: "Sobre mim",
     url: "#about",
-    isSelected: false,
+    isActive: false,
   },
   {
     title: "Experiências",
     url: "#experiences",
-    isSelected: false,
+    isActive: false,
   },
   {
     title: "Projetos",
     url: "#projects",
-    isSelected: false,
+    isActive: false,
   },
   {
     title: "Contato",
     url: "#contact",
-    isSelected: false,
+    isActive: false,
   },
 ];
 
 export default function Header() {
   return (
     <Box
-      width="100vw"
+      width="100%"
       position="fixed"
-      display="inline-flex"
+      display="flex"
       bgcolor={Colors.primary}
+      justifyContent="space-between"
+      alignItems="center"
+      paddingY={1.5}
+      zIndex={2000}
     >
-      <Box>
-        <img
+      <Box ml={3} zIndex={-1}>
+        <Image
           src={Logo}
           alt="Uma letra V e uma letra E escritas com caligrafia arredondada e sem serifas"
         />
       </Box>
-      <Box component="nav">
-        {anchors.map((anchor) => (
-          <
-          <a href={anchor.url}>{anchor.title}</a>
+      <Box component="nav" display="inline-flex" mr={3}>
+        {anchors.map((anchor, index) => (
+          <StyledLink key={index} isActive={anchor.isActive} href={anchor.url}>
+            {anchor.title}
+          </StyledLink>
         ))}
       </Box>
     </Box>
   );
 }
+
+const Image = styled.img`
+  max-height: 22px;
+`;
+
+const StyledLink = styled(Link)<isActivable>`
+  font-weight: bold;
+  color: ${Colors.white};
+  cursor: pointer;
+
+  ${({ isActive }) =>
+    isActive &&
+    `
+    color: ${Colors.secondary};
+
+    &::before {
+      content: "";
+      position: absolute;
+      width: auto;
+      height: 2px;
+      margin-top: 20px;
+      border-radius: 5px;
+      background-color: ${Colors.secondary};
+    }
+  `}
+
+  & + & {
+    margin-left: 15px;
+  }
+`;
