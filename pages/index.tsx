3702967c1f55992def5236d6adce94afeb6a4a33
Vinícius Ernani de Carvalho <vinicius.ernani_altran@prestador.globo>
2021-03-25 12:29:18 -0300

    fix: update footer import

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import Showcase from '../components/sections/showcase';
 import AboutMe from '../components/sections/about-me';
 import WorkExperiences from '../components/sections/work-experiences';
-import Footer from '../components/sections/footer/footer';
+import Footer from '../components/sections/footer';
 
 export default function Home() {
   return (
