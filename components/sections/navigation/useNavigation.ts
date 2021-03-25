dc201ba5d69aa9b9f9a2047c18b24ce7ebcb907d
Vinícius Ernani de Carvalho <vinicius.ernani_altran@prestador.globo>
2021-03-25 14:34:08 -0300

    feat: create the navigation  component

---
+++ components/sections/navigation/useNavigation.ts
@@ -0,0 +1,11 @@
+export default function useNavigation() {
+  const navigationPoins = [
+    { title: 'About me' },
+    { title: 'Work Experience' },
+    { title: 'My contacts' },
+  ];
+
+  return {
+    navigationPoins,
+  };
+}
